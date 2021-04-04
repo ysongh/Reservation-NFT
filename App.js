478226bba4e1b4eb9bbbf8940d5affbf388f45af
You Song Hou <yousonghou@yahoo.com>
2021-04-04 00:19:33 -0400

    set up the navigation with react-navigation

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ListScreen from './screens/ListScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Reservation-NFT</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+		<View style={styles.container}>
+			<Text>Reservation-NFT</Text>
+			<Button onPress={() => navigation.navigate('List')} title="Get Started" />
+		</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
