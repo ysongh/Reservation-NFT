a6962b786bb7d11ba0dc63e3c25b8f09dd2c2863
You Song Hou <yousonghou@yahoo.com>
2021-04-06 22:49:04 -0400

    set up the smart contract with truffle

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+const ContractKit = require('@celo/contractkit');
+const Web3 = require('web3');
+
+require('dotenv').config({path: '.env'});
+
+// Create connection to DataHub Celo Network node
+const web3 = new Web3(process.env.REST_URL);
+const client = ContractKit.newKitFromWeb3(web3);
+
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
+
+client.addAccount(account.privateKey);
+
+module.exports = {
+  networks: {
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    alfajores: {
+      provider: client.connection.web3.currentProvider, // CeloProvider
+      network_id: 44787  // latest Alfajores network id
+    }
+  },
+  contracts_build_directory: './server/abis/',
+};
