58f5c6f26791afca65b471b68fb7fb8e7204a703
You Song Hou <yousonghou@yahoo.com>
2021-04-09 23:23:11 -0400

    fix cors issues

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Server Work'));
 app.use('/api/user', require('./routes/user'));
