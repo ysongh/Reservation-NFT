9936306fa55f6fea4c5973a1e83d627501c2368c
You Song Hou <yousonghou@yahoo.com>
2021-04-14 22:49:36 -0400

    show Alfajores Celo explorer in a new browser after a success transaction

--- src/screens/CheckoutScreen.js
+++ src/screens/CheckoutScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, KeyboardAvoidingView, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, View, Linking } from 'react-native';
 import { Input, Button, Text } from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -10,6 +10,9 @@ export default function EventDetailScreen({ route, navigation }) {
   const [userData, setUserData] = useState({});
   const [name, setName] = useState("");
   const [balance, setBalance] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [transactionHash, setTransactionHash] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getBalance = async () => {
@@ -30,18 +33,22 @@ export default function EventDetailScreen({ route, navigation }) {
 
   const reservseEvent = async () => {
     try{
+      setLoading(true);
       const jsonValue = await AsyncStorage.getItem('@storage_Key');
 
-      const data = {
+      const userData = {
         privateKey: JSON.parse(jsonValue).privateKey,
         name: name
       }
 
-      const res = await axios.post(`/event/mintnft/${event._id}`, data);
-      navigation.replace('List');
+      const { data } = await axios.post(`/event/mintnft/${event._id}`, userData);
+      setTransactionHash(data.data.transactionHash);
+      setShowResult(true);
+      setLoading(false);
     }
     catch(err){
       console.error(err);
+      setLoading(false);
     }
   }
 
@@ -73,10 +80,11 @@ export default function EventDetailScreen({ route, navigation }) {
         onChangeText={(text) => setName(text)}
         clearButtonMode="while-editing"
       />
+      
+      { showResult ? <Text style={styles.success}>Success</Text> : <Button buttonStyle={{ backgroundColor: '#6643B5' }} onPress={() => reservseEvent()} disabled={loading} title="Pay" /> }
 
-      <View style={{marginVertical: 10}}></View>
-
-      <Button buttonStyle={{ backgroundColor: '#6643B5' }} onPress={() => reservseEvent()} title="Pay" />
+      { showResult && <Button buttonStyle={{ backgroundColor: '#6643B5', marginBottom: 5 }} onPress={() => Linking.openURL('https://alfajores-blockscout.celo-testnet.org/tx/' + transactionHash)} title="See Transaction" /> }
+      { showResult && <Button buttonStyle={{ backgroundColor: '#2794EB' }} onPress={() => navigation.navigate("List")} title="Back to List" /> }
     </KeyboardAvoidingView>
   );
 }
@@ -111,5 +119,11 @@ const styles = StyleSheet.create({
   detail: {
     fontSize: 17,
     marginVertical: 20,
+  },
+  success: {
+    fontSize: 27,
+    color: '#2794EB',
+    textAlign: 'center',
+    marginBottom: 15
   }
 });
